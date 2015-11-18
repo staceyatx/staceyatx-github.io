c67f67d27f619e7a733fc26f942167ed0bad1802
Emily DeVoll <emilydevollart@gmail.com>
2015-11-17 20:17:32 -0600

    Kindness Project researchsection added

--- js/main.js
+++ js/main.js
@@ -11,6 +11,12 @@ $('.parent-container1').magnificPopup({
   // other options
 });
 
+$('.parent-container2').magnificPopup({
+  delegate: 'a', // child items selector, by clicking on it popup will open
+  type: 'image'
+  // other options
+});
+
 $('.gallery-item').magnificPopup({
   type: 'image',
   gallery:{
